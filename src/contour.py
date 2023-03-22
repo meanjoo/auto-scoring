9989045341da8daa189b245bc601caa83fe86b8a
meanjoo <meanjooo@naver.com>
2023-03-22 17:24:08 +0900

    .

--- src/contour.py
+++ src/contour.py
@@ -37,27 +37,27 @@ class Rect:
     def __repr__(self):
         return repr((self.x, self.y, self.w, self.h))
 
-def is_rect_intersect(rect1, rect2):
-    minx = min(rect1.x, rect2.x)
-    miny = min(rect1.y, rect2.y)
-    maxx = max(rect1.x + rect1.w, rect2.x + rect2.w)
-    maxy = max(rect1.y + rect1.h, rect2.y + rect2.h)
+# def is_rect_intersect(rect1, rect2):
+#     minx = min(rect1.x, rect2.x)
+#     miny = min(rect1.y, rect2.y)
+#     maxx = max(rect1.x + rect1.w, rect2.x + rect2.w)
+#     maxy = max(rect1.y + rect1.h, rect2.y + rect2.h)
 
-    if maxx - minx <= rect1.w + rect2.w and maxy - miny <= rect1.h + rect2.h:
-        return True
-    return False
+#     if maxx - minx <= rect1.w + rect2.w and maxy - miny <= rect1.h + rect2.h:
+#         return True
+#     return False
 
-def is_rect_near_included(rect1, rect2):
-    pass
+# def is_rect_near_included(rect1, rect2):
+#     pass
 
-def minimum_area_contain_two_rects(rect1, rect2):
-    leftTop = Point(min(rect1.x, rect2.x), min(rect1.y, rect2.y))
-    rightBottom = Point(max(rect1.x + rect1.w, rect2.x + rect2.w), max(rect1.y + rect1.h, rect2.y + rect2.h))
+# def minimum_area_contain_two_rects(rect1, rect2):
+#     leftTop = Point(min(rect1.x, rect2.x), min(rect1.y, rect2.y))
+#     rightBottom = Point(max(rect1.x + rect1.w, rect2.x + rect2.w), max(rect1.y + rect1.h, rect2.y + rect2.h))
 
-    return Rect((leftTop.x, leftTop.y, rect1.w + rect2.w - (rightBottom.x - leftTop.x), rect1.h + rect2.h - (rightBottom.y - leftTop.y)))
+#     return Rect((leftTop.x, leftTop.y, rect1.w + rect2.w - (rightBottom.x - leftTop.x), rect1.h + rect2.h - (rightBottom.y - leftTop.y)))
 
 path = './test_image'
-img = cv.imread(path+'/sample.jpeg')
+img = cv.imread(path+'/newsample.jpeg')
 img2 = img.copy()
 
 if img is None:
@@ -70,88 +70,54 @@ gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 blur = cv.GaussianBlur(gray, (3,3),0)
 canny = cv.Canny(blur, 100, 200)
 
-
-contours, hier = cv.findContours(canny, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+row, col = 6,2
+contours, hier = cv.findContours(canny, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 # rects = [cv.boundingRect(each) for each in contours] # 사각형 정보 튜플(x,y,w,h)을 원소로 하는 리스트
 rects = [Rect(cv.boundingRect(each)) for each in contours] # cv.boundingRect(each)는 사각형 정보 튜플(x,y,w,h)을 반환
 rects.sort(key=lambda rect : (rect.x, rect.y, rect.w, rect.w))
 
-# area = [rect.w*rect.h for rect in rects]
-# print(area)
-# area.sort()
+crop, cropAll = [], []
+# 세로로 자르기
+for rect in rects:
+    tmp = (int)(rect.w / col)
+    for c in range(col):
+        crop.append(Rect((rect.x + tmp*c, rect.y, tmp, rect.h)))
 
-print('==rects==')
-print(rects)
+# 가로로 자르기
+for rect in crop:
+    tmp = (int)(rect.h / row)
+    for r in range(row):
+        cropAll.append(Rect((rect.x, rect.y + tmp*r, rect.w, tmp)))
+
+result = []
+for rect in cropAll:
+    croprect = canny[rect.y+rect.h:rect.x+rect.w]
+    contours, heir = cv.findContours(croprect, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE) # contours는 numpy.ndarray
+    result.extend(Rect(cv.boundingRect(each)) for each in contours)
+
+print('==crop==')
+print(crop)
+print('==cropAll==')
+print(cropAll)
+
+area = [rect.w*rect.h for rect in rects]
+area.sort()
+print('==area==')
+print(area)
 
 for rect in rects:
     color = (0,255,0)
     cv.rectangle(img, (rect.x, rect.y),
-                (rect.x + rect.w, rect.y + rect.h), color, 5)
+            (rect.x + rect.w, rect.y + rect.h), color, 5)
 
-crects = copy.deepcopy(rects)
-existIntersection = True
-while existIntersection and len(crects) > 1:
-    standardRect = crects[0]
-    slicerects = crects[1:]
-
-    tmp = []
-    existIntersection = False
-    # for sr in slicerects:
-    #     if is_rect_intersect(standardRect, sr):
-    #         tmp.append(minimum_area_contain_two_rects(standardRect, sr))
-    #         existIntersection = True
-    #     else:
-    #         tmp.append(sr)
-
-    for i in range(len(slicerects)):
-        if is_rect_intersect(standardRect, slicerects[i]):
-            rrr = minimum_area_contain_two_rects(standardRect, slicerects[i])
-            tmp.append(minimum_area_contain_two_rects(standardRect, slicerects[i]))
-            cv.rectangle(img2, (rrr.x, rrr.y),
-                (rrr.x + rrr.w, rrr.y + rrr.h), (255,0,0), 5)
-            existIntersection = True
-            tmp.extend(slicerects[i+1:])
-            break
-        else:
-            tmp.append(slicerects[i])
-    
-    if not existIntersection:
-        tmp.append(standardRect)
-    
-    crects = copy.deepcopy(tmp)
-
-
-print(len(rects))
-print(len(crects))
-
-# for rect in rects:
-#     # if not result:
-#     #     result.append(rect)
-#     #     continue
-#     result.append(rect).sort(key=lambda rect : (rect.x, rect.y, rect.w, rect.w))
-
-#     tmp = []
-
-#     # if abs(result[-1].x - rect.x) >= 10 or abs(result[-1].y - rect.y) >= 10:
-#     #     result.append(rect)
-#     #     continue
-
-#     if is_rect_intersect(result[-1], rect):
-#         t = result.pop()
-#         result.append(minimum_area_contain_two_rects(t, rect))
-#     else:
-#         result.append(rect)
-
-# print('==result==')
-# print(result)
-
-# print(len(rects))
-# print(len(result))
-
-for rect in crects:
+print('==result==')
+print(result)
+for rect in cropAll:
     color = (0,255,0)
     cv.rectangle(img2, (rect.x, rect.y),
-                (rect.x + rect.w, rect.y + rect.h), color, 5)
+            (rect.x + rect.w, rect.y + rect.h), color, 5)
+    
+
 
 k = cv.waitKey(0)
 
@@ -159,6 +125,6 @@ if k == 27:
     cv.destroyAllWindows()
 elif k == ord('s'):
     cv.imwrite('contourTest.jpg', img)
-    cv.imwrite('contourProcess.jpg', img2)
+    cv.imwrite('contourTest2.jpg', img2)
     
     cv.destroyAllWindows()
